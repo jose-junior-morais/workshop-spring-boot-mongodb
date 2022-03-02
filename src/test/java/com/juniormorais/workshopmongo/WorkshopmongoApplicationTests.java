1a36a32b72d38da31e4e7fb81d4014c065caa822
Jose Junior <juniormilian6@gmail.com>
2022-03-02 11:18:35 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/juniormorais/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniormorais.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
